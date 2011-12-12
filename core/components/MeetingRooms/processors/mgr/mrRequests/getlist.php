fd63c94b61ddccee5fb7704a6e0f2f2e03aaac59
unknown <chester@CHESTER-HP.rpl.org>
2011-12-12 13:30:54 -0600

    Grid displays data
    Signed-off-by: unknown <chester@CHESTER-HP.rpl.org>

---
+++ core/components/MeetingRooms/processors/mgr/mrRequests/getlist.php
@@ -0,0 +1,58 @@
+<?php
+$isLimit = !empty($scriptProperties['limit']);
+$start = $modx->getOption('start',$scriptProperties,0);
+$limit = $modx->getOption('limit',$scriptProperties,10);
+$sort = $modx->getOption('sort',$scriptProperties,'start');
+$dir = $modx->getOption('dir',$scriptProperties,'ASC');
+$query = $modx->getOption('query',$scriptProperties,'');
+$room = $modx->getOption('room',$scriptProperties,'');
+
+$mysqlFormat = 'Y-m-d H:i:s';
+/*
+//branch for when I add code to allow filtering appointments by room
+if (!empty $room) {
+
+
+} else {
+
+
+
+}
+
+//*/
+
+$c = $modx->newQuery('mrRequests');
+$date = date('Y-m-d 00:00:00');
+
+if (!empty($query)) {
+	$qstring = '%'.$query.'%';
+	$c->where(array(
+		'name:LIKE' => $qstring,
+		'OR:group:LIKE' => $qstring,
+		'OR:email:LIKE' => $qstring,
+		'OR:libraryCard:LIKE' => $qstring,
+		'OR:start:LIKE' => $qstring,
+		'OR:requestNumber:LIKE' => $qstring,
+		
+	));
+}
+$c->where(array(
+	'start:>=' => $date
+), xPDOQuery::SQL_AND);
+$count = $modx->getCount('mrRequess',$c);
+$c->sortby($sort,$dir);
+
+if ($isLimit) $c->limit($limit,$start);
+$requests = $modx->getIterator('mrRequests',$c);
+
+//iterate
+$list = array();
+foreach ($requests as $request) {
+	$requestArray = $request->toArray();
+	$room = $modx->getObject('mrRooms',$requestArray['room']);
+	$requestArray['room_name'] = $room->get('name');
+	$list[] = $requestArray;
+}
+return $this->outputArray($list,$count);
+
+		
