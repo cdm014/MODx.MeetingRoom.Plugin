6c4f9afb990206e2cb4bb1f6bcbcc8084d60bf87
Chester <chester@rpl.org>
2012-02-14 15:20:49 -0600

    testing how to get it to handle the resources

--- core/components/MeetingRooms/controllers/test.php
+++ core/components/MeetingRooms/controllers/test.php
@@ -1,8 +1,119 @@
 <?php
 require_once dirname(dirname(__FILE__)).'/model/MeetingRooms/mrmanager.class.php';
+
 $mrManager = new mrManager($modx);
 $modx->setLogLevel(modX::LOG_LEVEL_DEBUG);
 $modx->setLogTarget(XPDO_CLI_MODE ? 'ECHO' : 'HTML');
+$modx->regClientStartupScript($mrManager->config['jsUrl'].'mgr/MeetingRooms.js');
+$script1 = "<script type='text/javascript'>
+	MeetingRooms.config.connectorUrl = '/MeetingRooms/assets/components/MeetingRooms/connector.php';
+</script>";
+$modx->regClientStartupScript($script1);
+$bottomscripts = array();
+//*
+$script2 = <<<EOS
+<script type='text/javascript'>
+function makeRequest() {	
+	Ext.Ajax.request({
+		url: MeetingRooms.config.connectorUrl
+		,params: {
+			action: 'mgr/mrResources/getList'
+			,room: 1
+			,HTTP_MODAUTH: 'modx4ec2d5be8f9134.90501924_14f32d37264bd00.09031560'
+		}
+		,method: 'GET'
+		,success: function (result, request) {
+			Ext.MessageBox.alert('Success','Data Returned From Server: '+result.responseText);
+			//this allows us to convert the response to an object
+			eval("resources2 = "+result.responseText.replace("(",'').replace(")",''));
+			resources = resources2;
+			//alert(resources);
+			fields = new Array();
+			for (resource in resources.results) {
+				if (resources.results[resource].max_amount != undefined) {
+					field = new Object();
+					if (resources.results[resource].max_amount < 2) {
+						field.name = resources.results[resource].name;
+						field.xtype = 'checkbox';
+						alert("pause here");
+					} else {
+						field.name = resources.results[resource].name;
+						field.xtype = 'textfield';
+						
+					}
+					myform.add(field);
+				}
+			};
+			//alert(JSON.stringify(fields));
+			myform.doLayout();
+		}
+		,failure: function (result, request) {
+			Ext.MessageBox.alert('Failed', result.responseText);
+		}
+	
+	});
+}
+</script>
+EOS;
+//*/
+/*
+$script2 = <<<EOS
+<script type='text/javascript'>
+	alert("Message Box");
+</script>
+EOS;
+//*/
+//$modx->regClientStartupScript($script2);
+$bottomscripts [] = $script2;
+$bottomscripts[] = <<<EOS3
+<script type='text/javascript'>
+alert(resources);
+fields = new Array();
+for (resource in resources.results) {
+	if (resources.results[resource].max_amount != undefined) {
+		var field;
+		if (resources.results[resource].max_amount < 2) {
+			field.name = resources.results[resource].name;
+			field.type = 'checkbox';
+			
+		} else {
+			field.name = resources.results[resource].name;
+			field.type = 'text';
+			
+		}
+		fields.push(field);
+	}
+};
+alert(JSON.stringify(fields));
+</script>
+EOS3;
+
+$bottomscripts[] = <<<EOS4
+<script type="text/javascript">
+ myform = new Ext.form.FormPanel({
+	renderTo: 'forext'
+	,frame: false
+	,title: 'Resources Requested'
+	,width: 350
+	,defaults: {width: 230}
+	,defaultType: 'checkbox'
+	,items: [{
+		fieldLabel: 'Field1'
+		,name: 'field1'
+	},{
+		xtype: "button"
+		,text: "Add Resources"
+		,listeners: { 
+			'click': {fn: makeRequest}
+		}
+	}]
+
+	
+});
+myform.on("beforerender", "makeRequest()");
+</script>
+
+EOS4;
 $output = "Test";
 $mybasePath = 'c:/inetpub/wwwroot/test_site/';
 $myPath = 'MeetingRooms/core/components/MeetingRooms/';
@@ -47,4 +158,8 @@ $jsonresult = json_encode($list);
 $tests['json'] = '{"total":'.count($list).',"results":'.$jsonresult.'}';
 
 $output .= "<pre>".print_r($tests,true)."</pre>";
+$output .= "<div id='forext'></div>";
+foreach ($bottomscripts as $script) {
+	$output .= $script;
+}
 return $output;
